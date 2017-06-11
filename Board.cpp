e4fff007637b925f10015831d7cad05a1e570e92
Maxwell Carlson <bart4282819@gmail.com>
2017-06-10 20:45:39 -0700

    initial commit

---
+++ Board.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Board.cpp
+ * Author: C-60
+ * 
+ * Created on June 10, 2017, 8:39 PM
+ */
+
+#include "Board.h"
+
+Board::Board() {
+}
+
+Board::Board(const Board& orig) {
+}
+
+Board::~Board() {
+}
+
---
+++ Board.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Board.h
+ * Author: C-60
+ *
+ * Created on June 10, 2017, 8:39 PM
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+class Board {
+public:
+    Board();
+    Board(const Board& orig);
+    virtual ~Board();
+private:
+
+};
+
+#endif /* BOARD_H */
+
